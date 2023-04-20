33cf897626a7e8f668167c2e1bcc7446d36feaca
YasithCB <yasith.cb2001@gmail.com>
2023-04-20 16:00:50 +0530

    response util class added

---
+++ back-end/src/main/java/lk/carsnow/spring/util/ResponseUtil.java
@@ -0,0 +1,22 @@
+/**
+ * author  Yasith C Bandara
+ * created 4/19/2023 - 3:13 PM
+ * project back-end
+ */
+
+package lk.carsnow.spring.util;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
+public class ResponseUtil {
+    private int code;
+    private  String message;
+    private Object data;
+}
